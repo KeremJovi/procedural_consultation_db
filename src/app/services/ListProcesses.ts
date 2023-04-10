55a09fd58ffa64ffcda09080e9ad7519aa15a36d
KeremJovi <joviellysudre@hotmail.com>
2023-04-10 10:54:15 -0300

    feat: process listing service created

---
+++ src/app/services/ListProcesses.ts
@@ -0,0 +1,39 @@
+import Processes from "../model/Processes";
+
+
+
+export default class ListProcesses {
+
+  constructor(public numProcess: string | undefined, public court: string | undefined) { }
+
+  async list() {
+
+    const regexNumProcess = new RegExp(`.*${this.numProcess}.*`, "i")
+    const regexCourt = new RegExp(`.*${this.court}.*`, "i")
+
+    const filters = [];
+
+    if (this.numProcess) {
+      filters.push({
+        $and: [
+          {numProcess: regexNumProcess},
+          {numProcess: {$eq: this.numProcess}}
+        ]
+      });
+    }
+
+    if (this.court) {
+      filters.push({
+        $and: [
+          {court: regexCourt},
+          {court: {$eq: this.court}}
+        ]
+      });
+    }
+
+    const processes = await Processes.find(filters.length ? {$or: filters} : {});
+
+    return processes
+  }
+
+}

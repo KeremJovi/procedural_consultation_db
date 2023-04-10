ea968f39f852545a277078d9f1c264f56d5b7364
KeremJovi <joviellysudre@hotmail.com>
2023-04-10 10:50:44 -0300

    feat: created object to populate the database

---
+++ src/app/services/DTO/populete-Bank.ts
@@ -0,0 +1,351 @@
+import { IProcess } from "../../../typeConf"
+
+export const processesArray: IProcess[] = [
+  {
+    numProcess: "5001682-88.2020.8.13.0672",
+    court: 'TJSP',
+    description: "O assalto foi uma experiência traumática para mim. Eu nunca tinha me sentido tão vulnerável em minha vida. O ladrão apontou uma arma para mim e exigiu que eu entregasse minha carteira e celular. Depois disso, eu estava tremendo e me sentindo impotente.",
+    startDate: "2021-02-10",
+    movements: [
+      {
+        updateDate: "2021-02-13",
+        updateDescription: "Mas infelizmente, essa não é uma experiência incomum. Muitas pessoas são vítimas de assalto todos os dias. É uma situação que pode acontecer a qualquer um, em qualquer lugar. E o que é pior, muitas vezes não há nada que possamos fazer para evitá-la."
+      },
+      {
+        updateDate: "2021-02-21",
+        updateDescription: "O assalto é um crime violento e devastador. Além do trauma físico e emocional que causa à vítima, pode ter um impacto duradouro em sua vida. A sensação de medo e ansiedade pode persistir por semanas, meses ou até mesmo anos depois do evento. Algumas pessoas têm dificuldade em voltar a lugares onde se sentiram inseguras, ou desenvolvem transtornos como o transtorno de estresse pós-traumático."
+      },
+      {
+        updateDate: "2021-02-26",
+        updateDescription: "Além do impacto emocional, o assalto pode ter um impacto financeiro significativo. A perda de bens materiais como carteiras, celulares e joias pode ser substituída, mas muitas vezes há um custo maior envolvido. As vítimas podem ter que arcar com o custo de reparos em suas casas ou carros se forem danificados durante o assalto, ou podem precisar pagar por tratamentos médicos se forem feridos."
+      },
+      {
+        updateDate: "2021-03-10",
+        updateDescription: "Infelizmente, muitas pessoas acreditam que o assalto é um crime inevitável e que não há nada que possamos fazer para nos proteger. Mas isso não é verdade. Existem medidas que podemos tomar para reduzir o risco de sermos vítimas de um assalto. Algumas delas incluem evitar áreas perigosas, evitar carregar grandes quantias de dinheiro ou objetos de valor e estar sempre atento ao seu entorno. Além disso, é importante que as vítimas de assalto saibam que não estão sozinhas. Existem muitas organizações e grupos de apoio que podem ajudar a lidar com o trauma e o estresse após o evento. É importante buscar ajuda e apoio se você for vítima de assalto."
+      },
+      {
+        updateDate: "2021-03-22",
+        updateDescription: "Em resumo, o assalto é um crime devastador que pode ter um impacto duradouro na vida das vítimas. Mas é importante lembrar que existem medidas que podemos tomar para reduzir o risco de sermos vítimas e que há recursos disponíveis para ajudar a lidar com o trauma e o estresse após o evento."
+      },
+    ],
+    statusProcess: [
+      {
+        status: "Mas infelizmente, essa não é uma experiência incomum.",
+      },
+      {
+        status: "O assalto é um crime violento e devastador.",
+      },
+      {
+        status: "Além do impacto emocional, o assalto pode ter um impacto financeiro significativo.",
+      },
+      {
+        status: "Infelizmente, muitas pessoas acreditam que o assalto é um crime inevitável e que não há nada que possamos fazer para nos proteger.",
+      },
+      {
+        status: "Em resumo, o assalto é um crime devastador que pode ter um impacto duradouro na vida das vítimas.",
+      }
+    ],
+    creditor: {
+      name: "Sofia Oliveira",
+      link: "https://example.com/appellant/Sofia"
+    },
+    appellant: {
+      name: "Gabriel Souza",
+      link: "https://example.com/appellant/Gabriel"
+    },
+    attorney: [
+      {
+        name: "Laura Santos",
+        link: "https://example.com/appellant/Laura"
+      },
+      {
+        name: "Mateus Lima",
+        link: "https://example.com/appellant/Mateus"
+      },
+    ]
+  },
+  {
+    numProcess: "7003570-72.2021.8.22.0001",
+    court: 'TJRO',
+    description: "Fui vítima de um golpe financeiro, no qual fui levado a investir em um esquema fraudulento. Perdi todas as minhas economias e me encontro em uma situação financeira difícil. Sinto-me enganado e traído pela pessoa que me convenceu a investir.",
+    startDate: "2021-03-05",
+    movements: [
+      {
+        updateDate: "2021-03-08",
+        updateDescription: "Infelizmente, golpes financeiros como esse são cada vez mais comuns em todo o mundo. Muitas pessoas são enganadas por criminosos que se aproveitam da confiança e da ingenuidade delas. É importante estar sempre alerta e desconfiar de oportunidades de investimento que parecem boas demais para ser verdade."
+      },
+      {
+        updateDate: "2021-03-15",
+        updateDescription: "O golpe financeiro pode ter um impacto significativo na vida das vítimas. Além do impacto financeiro, pode afetar a saúde mental e emocional. As vítimas podem se sentir envergonhadas e humilhadas por terem sido enganadas, o que pode afetar sua autoestima e sua capacidade de confiar em outras pessoas no futuro."
+      },
+      {
+        updateDate: "2021-03-22",
+        updateDescription: "É importante lembrar que as vítimas de golpes financeiros não são culpadas pelo crime. Os golpistas são criminosos que se aproveitam da vulnerabilidade das pessoas. É importante denunciar o crime às autoridades e buscar ajuda para lidar com o impacto emocional e financeiro."
+      },
+      {
+        updateDate: "2021-04-01",
+        updateDescription: "Em resumo, os golpes financeiros são crimes que podem ter um impacto significativo na vida das vítimas. É importante estar sempre alerta e desconfiar de oportunidades de investimento que parecem boas demais para ser verdade. Se você for vítima de um golpe financeiro, é importante buscar ajuda e denunciar o crime às autoridades."
+      }
+    ],
+    statusProcess: [
+      {
+        status: "Infelizmente, golpes financeiros como esse são cada vez mais comuns em todo o mundo.",
+      },
+      {
+        status: "O golpe financeiro pode ter um impacto significativo na vida das vítimas.",
+      },
+      {
+        status: "É importante lembrar que as vítimas de golpes financeiros não são culpadas pelo crime.",
+      },
+      {
+        status: "Em resumo, os golpes financeiros são crimes que podem ter um impacto significativo na vida das vítimas.",
+      }
+    ],
+    creditor: {
+      name: "Paulo Ferreira",
+      link: "https://example.com/appellant/Paulo"
+    },
+    appellant: {
+      name: "Camila Silva",
+      link: "https://example.com/appellant/Camila"
+    },
+    attorney: [
+      {
+        name: "Luisa Oliveira",
+        link: "https://example.com/appellant/Luisa"
+      },
+      {
+        name: "Rafael Sousa",
+        link: "https://example.com/appellant/Rafael"
+      }
+    ]
+  },
+  {
+    numProcess: "5002321-44.2021.8.13.0672",
+    court: 'TJMG',
+    description: "Eu fui demitido injustamente do meu emprego. Eu sempre fui um funcionário dedicado e nunca recebi uma reclamação em toda a minha carreira. No entanto, meu chefe decidiu me demitir sem motivo aparente. Eu me sinto traído e injustiçado.",
+    startDate: "2021-08-05",
+    movements: [
+      {
+        updateDate: "2021-08-09",
+        updateDescription: "A demissão injusta é uma situação que pode acontecer com qualquer trabalhador. Infelizmente, muitas vezes não há nada que possamos fazer para evitá-la. É importante buscar ajuda e recursos para lidar com o estresse e a ansiedade após a demissão."
+      },
+      {
+        updateDate: "2021-08-17",
+        updateDescription: "Além do impacto emocional, a demissão injusta pode ter um impacto financeiro significativo. Os trabalhadores demitidos podem enfrentar dificuldades financeiras enquanto procuram por um novo emprego. É importante buscar ajuda e recursos para lidar com as despesas enquanto se procura por uma nova oportunidade."
+      },
+      {
+        updateDate: "2021-08-23",
+        updateDescription: "Infelizmente, muitas pessoas acreditam que a demissão injusta é inevitável e que não há nada que possamos fazer para nos proteger. Mas isso não é verdade. Existem medidas que podemos tomar para proteger nossos direitos como trabalhadores. É importante buscar ajuda e recursos para entender nossos direitos e lutar por eles se formos demitidos injustamente."
+      },
+      {
+        updateDate: "2021-09-05",
+        updateDescription: "Em resumo, a demissão injusta é uma situação devastadora que pode ter um impacto duradouro na vida dos trabalhadores. Mas é importante lembrar que existem medidas que podemos tomar para proteger nossos direitos e buscar ajuda e recursos para lidar com o estresse e a ansiedade após a demissão."
+      },
+    ],
+    statusProcess: [
+      {
+        status: "A demissão injusta é uma situação que pode acontecer com qualquer trabalhador.",
+      },
+      {
+        status: "Além do impacto emocional, a demissão injusta pode ter um impacto financeiro significativo.",
+      },
+      {
+        status: "Infelizmente, muitas pessoas acreditam que a demissão injusta é inevitável e que não há nada que possamos fazer para nos proteger.",
+      },
+      {
+        status: "Em resumo, a demissão injusta é uma situação devastadora que pode ter um impacto duradouro na vida dos trabalhadores.",
+      }
+    ],
+    creditor: {
+      name: "Lucas Oliveira",
+      link: "https://example.com/appellant/Lucas"
+    },
+    appellant: {
+      name: "Jéssica Souza",
+      link: "https://example.com/appellant/Jéssica"
+    },
+    attorney: [
+      {
+        name: "Ana Santos",
+        link: "https://example.com/appellant/Ana"
+      },
+      {
+        name: "Pedro Almeida",
+        link: "https://example.com/appellant/Pedro"
+      }
+    ]
+  },
+  {
+    numProcess: "5002857-30.2021.8.13.0672",
+    court: 'TJSP',
+    description: "Fui vítima de um golpe na internet. Comprei um produto em um site que parecia legítimo, mas nunca recebi a encomenda. Quando tentei entrar em contato com a empresa, descobri que não havia telefone ou endereço físico disponível. Perdi meu dinheiro e fiquei frustrado com a situação.",
+    startDate: "2021-04-01",
+    movements: [
+      {
+        updateDate: "2021-04-05",
+        updateDescription: "Golpes na internet são uma realidade cada vez mais comum. É importante ter cuidado ao fazer compras online e verificar a reputação da empresa antes de fazer uma compra. Além disso, é importante verificar se o site tem um endereço físico e um número de telefone de contato."
+      },
+      {
+        updateDate: "2021-04-10",
+        updateDescription: "Se você foi vítima de um golpe na internet, é importante registrar uma reclamação no Procon ou em outras instituições de defesa do consumidor. Além disso, é possível denunciar o site fraudulento às autoridades competentes, para que outras pessoas não sejam vítimas do mesmo golpe."
+      },
+      {
+        updateDate: "2021-04-15",
+        updateDescription: "Infelizmente, nem sempre é possível recuperar o dinheiro perdido em um golpe na internet. No entanto, é possível tomar medidas para evitar ser vítima novamente no futuro. Uma delas é usar formas de pagamento seguras, como cartões de crédito ou sistemas de pagamento online com proteção ao consumidor."
+      },
+      {
+        updateDate: "2021-04-20",
+        updateDescription: "Em resumo, os golpes na internet são uma realidade preocupante, mas é possível tomar medidas para se proteger. Verifique a reputação da empresa antes de fazer uma compra, use formas de pagamento seguras e denuncie os golpes às autoridades competentes se for vítima de um. "
+      },
+    ],
+    statusProcess: [
+      {
+        status: "Golpes na internet são uma realidade cada vez mais comum."
+      },
+      {
+        status: "Se você foi vítima de um golpe na internet, é importante registrar uma reclamação no Procon ou em outras instituições de defesa do consumidor."
+      },
+      {
+        status: "Infelizmente, nem sempre é possível recuperar o dinheiro perdido em um golpe na internet."
+      },
+      {
+        status: "Em resumo, os golpes na internet são uma realidade preocupante, mas é possível tomar medidas para se proteger."
+      }
+    ],
+    creditor: {
+      name: "João Santos",
+      link: "https://example.com/appellant/Joao"
+    },
+    appellant: {
+      name: "Marcela Silva",
+      link: "https://example.com/appellant/Marcela"
+    },
+    attorney: [
+      {
+        name: "Carla Lima",
+        link: "https://example.com/appellant/Carla"
+      },
+      {
+        name: "Gustavo Pereira",
+        link: "https://example.com/appellant/Gustavo"
+      },
+    ]
+  },
+  {
+    numProcess: "3001142-45.2022.8.12.0024",
+    court: 'TJMS',
+    description: "Eu estava andando na rua quando um cachorro começou a latir e correr em minha direção. Fiquei com muito medo e comecei a andar mais rápido, mas o cachorro continuou a me seguir. Ele finalmente pulou em mim e me derrubou no chão. Eu estava ferido e assustado, mas felizmente uma pessoa próxima ouviu meus gritos e veio em meu auxílio. O cachorro foi capturado e levado para o abrigo de animais da cidade.",
+    startDate: "2022-03-15",
+    movements: [
+      {
+        updateDate: "2022-03-18",
+        updateDescription: "Os ataques de cães são uma preocupação comum para muitas pessoas. Infelizmente, mesmo os cães mais amigáveis podem se tornar agressivos se se sentirem ameaçados ou se não forem treinados corretamente. É importante estar sempre atento ao seu entorno e evitar aproximação de cães desconhecidos ou soltos."
+      },
+      {
+        updateDate: "2022-03-22",
+        updateDescription: "Os ataques de cães podem ser extremamente traumatizantes e podem resultar em ferimentos graves. Além do trauma físico, as vítimas podem experimentar sentimentos de medo, ansiedade e fobia de cães após o incidente. É importante buscar ajuda profissional para lidar com esses sentimentos e evitar desenvolver transtornos de ansiedade."
+      },
+      {
+        updateDate: "2022-03-28",
+        updateDescription: "Se você é proprietário de um cachorro, é importante garantir que ele seja treinado e socializado adequadamente. Você também deve estar ciente das leis locais em relação aos cães e mantê-los em áreas designadas e adequadas para animais de estimação. Tomar essas medidas pode ajudar a prevenir ataques de cães e manter a segurança das pessoas e outros animais de estimação."
+      },
+    ],
+    statusProcess: [
+      {
+        status: "Os ataques de cães são uma preocupação comum para muitas pessoas.",
+      },
+      {
+        status: "Os ataques de cães podem ser extremamente traumatizantes e podem resultar em ferimentos graves.",
+      },
+      {
+        status: "Se você é proprietário de um cachorro, é importante garantir que ele seja treinado e socializado adequadamente.",
+      },
+      {
+        status: "Tomar essas medidas pode ajudar a prevenir ataques de cães e manter a segurança das pessoas e outros animais de estimação.",
+      },
+    ],
+    creditor: {
+      name: "Juliana Oliveira",
+      link: "https://example.com/appellant/Juliana"
+    },
+    appellant: {
+      name: "Lucas Souza",
+      link: "https://example.com/appellant/Lucas"
+    },
+    attorney: [
+      {
+        name: "Isabella Santos",
+        link: "https://example.com/appellant/Isabella"
+      },
+      {
+        name: "Maurício Lima",
+        link: "https://example.com/appellant/Mauricio"
+      },
+    ]
+  },
+  {
+    numProcess: "5002568-72.2022.8.13.0672",
+    court: 'TJMG',
+    description: "Eu estava dirigindo para casa depois de um longo dia de trabalho quando fui atingido por outro carro. Eu não estava esperando isso e não tive tempo para reagir. Foi um acidente horrível e eu me sinto sortudo por ter sobrevivido. Agora, estou lidando com as consequências do acidente e tentando me recuperar.",
+    startDate: "2022-01-15",
+    movements: [
+      {
+        updateDate: "2022-01-18",
+        updateDescription: "Acidentes de carro podem acontecer com qualquer um de nós a qualquer momento. É importante estar preparado para essas situações e saber o que fazer em caso de acidente. Também é importante dirigir com cuidado e atenção para evitar acidentes sempre que possível."
+      },
+      {
+        updateDate: "2022-01-25",
+        updateDescription: "Os acidentes de carro podem ter consequências graves, tanto físicas quanto emocionais. Lesões podem exigir tratamento médico e reabilitação, e as consequências emocionais podem ser igualmente desafiadoras. É importante procurar apoio e ajuda se você estiver enfrentando dificuldades após um acidente de carro."
+      },
+      {
+        updateDate: "2022-02-05",
+        updateDescription: "Além dos impactos físicos e emocionais, os acidentes de carro também podem ter um impacto financeiro significativo. Os custos médicos, reparos de carro e tempo perdido no trabalho podem se acumular rapidamente. É importante garantir que você tenha seguro adequado e proteção financeira para lidar com esses custos, se necessário."
+      },
+      {
+        updateDate: "2022-02-20",
+        updateDescription: "Em casos de acidentes de carro, pode ser necessário contratar um advogado para ajudá-lo a proteger seus direitos e obter a compensação adequada pelos danos que você sofreu. É importante escolher um advogado experiente e qualificado que possa ajudá-lo a navegar pelo sistema legal e obter um resultado justo em seu caso."
+      },
+      {
+        updateDate: "2022-03-10",
+        updateDescription: "Em resumo, os acidentes de carro podem ter consequências graves e duradouras. É importante estar preparado, dirigir com cuidado e procurar ajuda se você precisar. Se você sofrer um acidente de carro, lembre-se de que você tem direitos e opções, e que há recursos disponíveis para ajudá-lo a lidar com os desafios que você enfrenta."
+      },
+    ],
+    statusProcess: [
+      {
+        status: "Os acidentes de carro podem acontecer com qualquer um de nós a qualquer momento.",
+      },
+      {
+        status: "Os acidentes de carro podem ter consequências graves, tanto físicas quanto emocionais.",
+      },
+      {
+        status: "Os acidentes de carro também podem ter um impacto financeiro significativo.",
+      },
+      {
+        status: "Em casos de acidentes de carro, pode ser necessário contratar um advogado para ajudá-lo a proteger seus direitos e obter a compensação adequada pelos danos que você sofre."
+      }
+    ],
+    creditor: {
+      name: "Juliana Oliveira",
+      link: "https://example.com/appellant/Juliana"
+    },
+    appellant: {
+      name: "Lucas Souza",
+      link: "https://example.com/appellant/Lucas"
+    },
+    attorney: [
+      {
+        name: "Isabella Santos",
+        link: "https://example.com/appellant/Isabella"
+      },
+      {
+        name: "Maurício Lima",
+        link: "https://example.com/appellant/Mauricio"
+      },
+    ]
+  }
+
+
+]
+
+

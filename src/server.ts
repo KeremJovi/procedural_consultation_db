d9efde48f8f85cf8ca68fb270463d3232b6554e7
KeremJovi <joviellysudre@hotmail.com>
2023-04-11 08:16:36 -0300

    feat: adding error handling middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
 import app from './app'
 import 'express-async-errors';
+import HandlingError from './midlleware/Error/HandlingError';
+import {NextFunction, Request, Response, response} from 'express';
+
+const port = process.env.PORT || 3030;
 
 
-const port = process.env.PORT || 3000;
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof HandlingError) {
+    return res.status(error.statusCode).send({
+      statusCode: error.statusCode,
+      message: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
+})
 
 
 app.listen(port, () => {
